9fa6e2ac8e337a964004efceab513f09abc99fc1
Nidhi Sharma <you@example.com>
2025-01-05 18:35:43 -0500

    feat: Add frontend routes

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/home/Home";
+import HotelList from "./pages/hotelList/HotelList";
+import Hotel from "./pages/hotel/Hotel";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<HotelList />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import Container from "@mui/material/Container";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Container>sd</Container>
+    </div>
+  );
+};
+
+export default Navbar;
